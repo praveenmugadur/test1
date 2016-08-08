b797a93ab436a2dbf9f35953a4c0f725e320e844
mohan31 <31mohan31@gmail.com>
2016-08-08 16:28:17 +0530

    Update test.java
    version4.0

--- test.java
+++ test.java
@@ -3,6 +3,8 @@ public static void main(String a[])
 {
 System.out.println("stock");
 System.out.println("version1.0");
+System.out.println("stockversion1.0");
 System.out.println("version1.1");
+System.out.println("version2.0");
 }
 }
